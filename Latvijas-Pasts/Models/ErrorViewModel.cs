05f8682145ec997069c70113cfb7cad31e7d7631
Renards <renards.kr@gmail.com>
2024-11-08 15:15:51 +0200

    Created Project

---
+++ Latvijas-Pasts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Latvijas_Pasts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
